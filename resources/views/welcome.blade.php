031bac4d49a625a8ea320934ae6b2de1a864130d
ThePFMind <thepfmind@gmail.com>
2016-01-18 22:57:26 +0800

    Init Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Laravel + Foundation</title>
+
+  <link href="/css/app.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="row">
+
+  <div>
+    <h1 class="text-center">Laravel 5 + foundation</h1>
+  </div>
+
+</div>
+</body>
+</html>
